a1c0bc3da6ed156dc13e3d88b496cb7a125c6fe7
Alanoliveiramedeiros <alanoliveiramedeiros12@gmail.com>
2025-03-25 21:29:54 -0300

    Atividade - 07 - javascript

---
+++ Atividade 06 - javascript/script.js
@@ -0,0 +1,37 @@
+//let nome;
+//nome = "alan"
+//nome = "matheus"
+//nome = "joão"
+
+//let idade = 20
+
+//const pih = 3.1415
+
+//alert("Helo Word")
+//confirm("Esta gostando do JS?")
+//nome = prompt("Qual seu nome?")
+//idade = prompt("Qual sua idade?")
+//document.writeln("Olá, " + nome + " sua idade é " + idade + " <br> ") 
+//document.writeln(`Olá, ${nome} sua idade é ${idade}`)
+
+
+//let nr1 = Number(prompt("Informe o primeiro numero"))
+//let nr2 = Number(prompt("Informe o segundo numero"))
+
+//let soma
+//soma = nr1 + nr2
+
+//document.writeln(`Valor da sua soma é: ${nr1} + ${nr2} = ${soma}`)
+
+
+
+let nt1 = Number(prompt("Informe o primeiro nota?"))
+let nt2 = Number(prompt("Informe o segunda nota?"))
+let nt3 = Number(prompt("Informe o terceira nota? "))
+
+let soma
+soma = (nt1 + nt2 + nt3) /3
+document.writeln(`Sua Média é: ${soma}`)
+
+
+
